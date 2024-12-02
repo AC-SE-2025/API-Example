db9bc7206e605efec460fc9ff8ddb2df0e1e4476
Michael Kelly <92009909+mkelly133@users.noreply.github.com>
2024-12-02 05:57:50 +0000

    Implemented SQLAlchemy and Login

--- app.py
+++ app.py
@@ -1,47 +1,77 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite database
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-# Sample data: a list of fruits
-fruits = [
-    {"id": 1, "name": "Apple"},
-    {"id": 2, "name": "Banana"},
-]
+# User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
 
-# Route to serve the HTML page
+# Fruit model
+class Fruit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
 def index():
+    fruits = Fruit.query.all()
     return render_template('index.html', fruits=fruits)
 
-# Get the list of fruits
-@app.route('/fruits', methods=['GET'])
-def get_fruits():
-    return jsonify(fruits)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
 
-# Add a new fruit
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/fruits', methods=['GET'])
+@login_required
+def get_fruits():
+    fruits = Fruit.query.all()
+    return jsonify([{'id': fruit.id, 'name': fruit.name} for fruit in fruits])
+
 @app.route('/fruits', methods=['POST'])
+@login_required
 def add_fruit():
     new_fruit = request.form
     fruit_name = new_fruit['name']
-    fruit_id = len(fruits) + 1
-    fruits.append({"id": fruit_id, "name": fruit_name})
-    return jsonify({"id": fruit_id, "name": fruit_name}), 201
-
-# Update an existing fruit
-@app.route('/fruits/<int:fruit_id>', methods=['PUT'])
-def update_fruit(fruit_id):
-    fruit = next((fruit for fruit in fruits if fruit['id'] == fruit_id), None)
-    if fruit is None:
-        return jsonify({'error': 'Fruit not found'}), 404
-    updated_data = request.form
-    fruit.update(updated_data)
-    return jsonify(fruit)
-
-# Delete a fruit
-@app.route('/fruits/<int:fruit_id>', methods=['DELETE'])
-def delete_fruit(fruit_id):
-    global fruits
-    fruits = [fruit for fruit in fruits if fruit['id'] != fruit_id]
-    return jsonify({'message': 'Fruit deleted'}), 204
+    fruit = Fruit(name=fruit_name)
+    db.session.add(fruit)
+    db.session.commit()
+    return jsonify({"id": fruit.id, "name": fruit.name}), 201
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create all database tables
+        try:
+            user = User(username='admin', password=generate_password_hash('password'))
+            db.session.add(user)
+            db.session.commit()
+        except:
+            pass 
     app.run(debug=True)
